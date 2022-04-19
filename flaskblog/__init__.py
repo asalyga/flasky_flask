60c7ec28ba99db1cd9830b67819e9595b504abbf
asalyga <aleksander.salyga@gmail.com>
2022-04-19 22:56:22 +0200

    Initial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '0aac8db6fa117e5b6399332aa4ca07bc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
